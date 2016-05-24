f83f8132b76f51f4923bf00039bdbcb0844e453a
Thomas Wittek <thomas@wittek.me>
2016-05-24 20:26:52 +0200

    Adapt to new XML. Better logging. Filter out podcasts.

--- cleanup.py
+++ cleanup.py
@@ -14,15 +14,15 @@ LOG_FORMAT = '%(message)s'
 logger = logging.getLogger(__name__)
 
 
-def delete_crappy_singles(all_tracks, albums, min_rating=80, dryrun=True):
+def delete_crappy_singles(songs, albums, min_rating=80, dryrun=True):
     logger.info('Deleting crappy singles...')
-    crappy_singles = analysis.find_crappy_single_tracks(all_tracks, albums, min_rating)
+    crappy_singles = analysis.find_crappy_single_tracks(songs, albums, min_rating)
     # 10 means 0 stars in iTunes. We consider 0 stars as unrated.
     for track in crappy_singles:
-        location = track.location.replace('file://localhost', '')
-        logger.info(location)
+        location = track.location.replace('file://', '')
+        logger.info('Deleting ' + location)
         if dryrun:
-            print 'rm "%s"' % location
+            print ('rm "%s"' % location).encode('utf-8')
         else:
             os.remove(location)
 
@@ -47,7 +47,7 @@ def delete_compilations(
 
 def delete_albums(albums, min_rating=80, keep_good_tracks=True, dryrun=True):
     for album in albums:
-        source = album.tracks[0].location.replace('file://localhost', '')
+        source = album.tracks[0].location.replace('file://', '')
         source_dir = os.path.dirname(source)
         if not os.path.exists(source_dir):
             logger.warning('Path does not exist: %s.', source_dir)
@@ -56,19 +56,20 @@ def delete_albums(albums, min_rating=80, keep_good_tracks=True, dryrun=True):
         source_parent = os.path.split(source_dir)[0]
 
         for track in album.tracks:
-            source = track.location.replace('file://localhost', '')
-            logger.info(source)
+            source = track.location.replace('file://', '')
             if track.rating >= min_rating:
                 if keep_good_tracks:
                     target_dir = source_parent
                     target_file = '%s - %s.mp3' % (track.artist, track.name)
                     target = os.path.join(target_dir, target_file)
                     if dryrun:
+                        logger.info('Moving ' + source)
                         print ('mv "%s" "%s"' % (source, target)) \
                                 .encode('utf-8')
                     else:
                         os.rename(source, target)
             else:
+                logger.info('Deleting ' + source)
                 if dryrun:
                     print ('rm "%s"' % source).encode('utf-8')
                 else:
@@ -88,19 +89,18 @@ def main():
     logger.info('Opening iTunes library...')
     lib = pytunes.Library()
     logger.info('Loading all tracks...')
-    all_tracks = list(lib.tracks)
-    albums = list(pytunes.Album.group_tracks_into_albums(all_tracks))
+    songs = filter(lambda t: not t.podcast, lib.tracks)
+    albums = list(pytunes.Album.group_tracks_into_albums(songs))
     dryrun = True
-    n = 20
     min_rating = 80  # 4 stars.
     min_good_tracks = 4
     keep_good_tracks = True
     tolerated_time_difference = 10
 
-    #delete_crappy_singles(all_tracks, albums, min_rating, dryrun)
-    #delete_crappy_albums(
-    #        albums, min_good_tracks, min_rating, keep_good_tracks, dryrun)
-    #delete_compilations(albums, min_rating, keep_good_tracks, dryrun)
+    delete_crappy_singles(songs, albums, min_rating, dryrun)
+    delete_crappy_albums(
+            albums, min_good_tracks, min_rating, keep_good_tracks, dryrun)
+    delete_compilations(albums, min_rating, keep_good_tracks, dryrun)
 
 
 if __name__ == '__main__':
